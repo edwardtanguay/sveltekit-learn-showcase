b1ee22370aa30e9d3b59688dbfc0d05a6027d390
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 01:31:38 +0200

    dynamic start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,18 @@
 	import Example from '../components/Example.svelte';
 	import Header from '../components/Header.svelte';
 	import Ex001Test from '../components/examples/Ex001Test.svelte';
-	import Ex002DynamicImages from '../components/examples/Ex002DynamicImages.svelte';
+	import Ex002StaticImages from '../components/examples/Ex002StaticImages.svelte';
+	import Ex003DynamicImages from '../components/examples/Ex003DynamicImages.svelte';
+	import Ex002DynamicImages from '../components/examples/Ex003DynamicImages.svelte';
 </script>
 
 <Header />
 <Example title="Ex001Test">
 	<Ex001Test />
 </Example>
-<Example title="Ex002DynamicImages">
-	<Ex002DynamicImages/>
+<Example title="Ex002StaticImages">
+	<Ex002StaticImages />
+</Example>
+<Example title="Ex003DynamicImages">
+	<Ex003DynamicImages/>
 </Example>
