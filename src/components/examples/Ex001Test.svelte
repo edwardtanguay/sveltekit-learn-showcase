216801b01cee92820d00d8bdceb653a3dd6bce41
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 01:11:14 +0200

    simple ex001

--- src/components/examples/Ex001Test.svelte
+++ src/components/examples/Ex001Test.svelte
@@ -1,1 +1,14 @@
-<p>test</p>
+<script lang="ts">
+	let count = 0;
+
+	$: doubled = count * 2;
+
+	const increment = () => {
+		count++;
+	};
+</script>
+
+<button on:click={increment} class="bg-gray-300 px-2 pb-1 rounded border border-gray-500"
+	>count: {count}</button
+>
+<p class=" pt-1">doubled: {doubled}</p>
