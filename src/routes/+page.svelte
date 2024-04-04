e0c503081f8591d60990844e2b4b682ce26e190a
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 22:45:02 +0200

    now using layouts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import Example from '../components/Example.svelte';
-	import Header from '../components/Header.svelte';
 	import SiteInfo from '../components/SiteInfo.svelte';
 	import img from '$lib/images/sveltekit.jpg';
 </script>
 
-<Header />
 <SiteInfo />
-
 <h2 class="text-xl mt-3">Currently learning SvelteKit</h2>
 <img class="mb-1 w-[20rem] rounded-lg mt-2" src={img} alt="developer" />
--- src/routes/basics/+page.svelte
+++ src/routes/basics/+page.svelte
@@ -1,12 +1,10 @@
 <script>
 	import Example from '../../components/Example.svelte';
-	import Header from '../../components/Header.svelte';
 	import Ex001Test from '../../components/examples/Ex001Test.svelte';
 	import Ex002StaticImages from '../../components/examples/Ex002StaticImages.svelte';
 	import Ex003DynamicImages from '../../components/examples/Ex003DynamicImages.svelte';
 </script>
 
-<Header />
 <Example title="Ex001Test">
 	<Ex001Test />
 </Example>
