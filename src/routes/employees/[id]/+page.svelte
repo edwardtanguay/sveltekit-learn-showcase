23c00533451fb7cd48dce9ab59f9d26c7be38641
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 23:48:05 +0200

    showing lastname

--- src/routes/employees/[id]/+page.svelte
+++ src/routes/employees/[id]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import employees from '../../../data/employees.json';
 	import {page} from '$app/stores';
+
+	const employee = employees.find(m => String(m.employeeID) === $page.params.id)
 </script>
 
-<p>emp id: {$page.params.id}</p>
+<p>{employee?.lastName}</p>
