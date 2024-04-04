dafd51959ae03ada67163a64d4f823bf03cdca37
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 23:43:31 +0200

    showing employee list

--- src/routes/employees/+page.svelte
+++ src/routes/employees/+page.svelte
@@ -2,4 +2,8 @@
 	import employees from '../../data/employees.json';
 </script>
 
-<p>There are {employees.length} employees.</p>
+<ul class="list-disc ml-6">
+{#each employees as employee}
+	<li><a href={`/employees/${employee.employeeID}`} class="underline">{employee.firstName} {employee.lastName}</a></li>
+{/each}
+</ul>
