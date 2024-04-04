fed28eaf9a47f53faf2dff9bf1af69ccc65e6a84
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 01:01:21 +0200

    responsive 2

--- src/components/SiteInfo.svelte
+++ src/components/SiteInfo.svelte
@@ -1,15 +1,15 @@
-<h2 class="text-xl">Site Info</h2>
+<h2 class="text-xl">Site Links</h2>
 <ul class="list-disc ml-6">
 	<li>
-		repo: <a
+		<a
 			class="underline"
 			href="https://github.com/edwardtanguay/sveltekit-learn-showcase"
-			target="_blank">https://github.com/edwardtanguay/sveltekit-learn-showcase</a
+			target="_blank">repository</a
 		>
 	</li>
 	<li>
-		live: <a class="underline" href="https://sveltekit-learn-showcase.vercel.app" target="_blank"
-			>https://sveltekit-learn-showcase.vercel.app</a
+		<a class="underline" href="https://sveltekit-learn-showcase.vercel.app" target="_blank"
+			>live site</a
 		>
 	</li>
 </ul>
