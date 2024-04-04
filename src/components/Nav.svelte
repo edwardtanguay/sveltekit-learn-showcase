b42004be4ef488635d16ac0b1a3d6ad986712562
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 22:11:14 +0200

    pages work with color

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+
+	console.log('pathname', $page.url.pathname);
+</script>
+
 <nav class="bg-slate-600 px-3 py-1 mb-3 rounded text-slate-200 flex gap-2">
-	<a href="/">Welcome</a>
-	<a href="/basics">Basics</a>
+	<a class:active={$page.url.pathname === '/'} href="/">Welcome</a>
+	<a class:active={$page.url.pathname === '/basics'} href="/basics">Basics</a>
 </nav>
+
+<style>
+	a.active {
+		color: rgb(253, 144, 94) !important;
+	}
+</style>
