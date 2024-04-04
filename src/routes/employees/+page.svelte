3e57d68be7fe0f030936f3107d96c2f749ef3ae5
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 01:28:49 +0200

    9 tests

--- src/routes/employees/+page.svelte
+++ src/routes/employees/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <ul class="list-disc ml-6">
-<li>test</li>
-<!-- {#each employees as employee}
-	<li class="mb-1"><a href={`/employees/${employee.employeeID}`} class="underline">{employee.firstName} {employee.lastName}</a></li>
-{/each} -->
+{#each employees as employee}
+	<!-- <li class="mb-1"><a href={`/employees/${employee.employeeID}`} class="underline">{employee.firstName} {employee.lastName}</a></li> -->
+	<li>test</li>
+{/each}
 </ul>
