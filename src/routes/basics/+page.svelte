8afc004c2dffce335a0c9bab6e7a1f52aa2312fa
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 01:20:54 +0200

    again

--- src/routes/basics/+page.svelte
+++ src/routes/basics/+page.svelte
@@ -3,17 +3,8 @@
 	import Ex001Test from '../../components/examples/Ex001Test.svelte';
 	import Ex002StaticImages from '../../components/examples/Ex002StaticImages.svelte';
 	import Ex003DynamicImages from '../../components/examples/Ex003DynamicImages.svelte';
-
-	let message = 'this is the message';
-	const changeMessage = () => {
-		message = 'message was changed';
-	};
 </script>
 
-<!-- {message}
-<div>
-	<button on:click={changeMessage} class="bg-gray-300 px-2 py-1 rounded mt-2">change the message</button>
-</div> -->
 <Example title="Ex001Test">
 	<Ex001Test />
 </Example>
