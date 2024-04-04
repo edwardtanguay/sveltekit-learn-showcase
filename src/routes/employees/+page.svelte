6b61416351318dde76f637ee80c82d376f21ef97
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 01:39:25 +0200

    firstlast

--- src/routes/employees/+page.svelte
+++ src/routes/employees/+page.svelte
@@ -5,6 +5,6 @@
 <ul class="list-disc ml-6">
 {#each employees as employee}
 	<!-- <li class="mb-1"><a href={`/employees/${employee.employeeID}`} class="underline">{employee.firstName} {employee.lastName}</a></li> -->
-	<li>{employee.firstName}</li>
+	<li>{employee.firstName} {employee.lastName}</li>
 {/each}
 </ul>
