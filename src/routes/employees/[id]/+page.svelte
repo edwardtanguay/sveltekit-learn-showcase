e899825252398ce9b1efb2920b617086b2bf7e75
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 23:50:31 +0200

    catches 404

--- src/routes/employees/[id]/+page.svelte
+++ src/routes/employees/[id]/+page.svelte
@@ -2,7 +2,11 @@
 	import employees from '../../../data/employees.json';
 	import {page} from '$app/stores';
 
-	const employee = employees.find(m => String(m.employeeID) === $page.params.id)
+	const employee = employees.find(m => String(m.employeeID) === $page.params.id);
 </script>
 
+{#if employee}
 <p>{employee?.lastName}</p>
+{:else}
+<p>ID "{$page.params.id}" does not exist, see <a href="/employees" class="underline">employee list</a>.</p>
+{/if}
