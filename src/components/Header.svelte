e7978f7385ba3427bf0aec338e46631d24d6d90a
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 01:02:47 +0200

    header respons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,5 +2,5 @@
 	import Nav from './Nav.svelte';
 </script>
 
-<h1 class="text-orange-800 text-3xl mb-2">SvelteKit Learn Showcase</h1>
+<h1 class="text-orange-800 text-[1.6rem] mb-2">SvelteKit Learn Showcase</h1>
 <Nav />
