3809a28cb1754a44eac0f00e59e90d679a23be61
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 23:33:27 +0200

    slugs being read

--- src/routes/blog/[slug]/page.ts
+++
@@ -1,7 +0,0 @@
-import type { RouteParams } from "../../../types";
-
-export const load = ({ params }: { params: RouteParams }) => {
-	return {
-		slug: params.slug
-	};
-};
--- src/types.ts
+++
@@ -1,3 +0,0 @@
-export type RouteParams = {
-	slug: string;
-};
