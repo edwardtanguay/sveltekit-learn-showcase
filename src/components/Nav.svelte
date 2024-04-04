29780b2ecb7467a5b237f97e6646a76cce924609
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 22:20:51 +0200

    dynamic nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,12 +1,22 @@
 <script>
 	import { page } from '$app/stores';
 
-	console.log('pathname', $page.url.pathname);
+	const pageItems = [
+		{
+			href: '/',
+			title: 'Home'
+		},
+		{
+			href: '/basics',
+			title: 'Basics'
+		}
+	];
 </script>
 
-<nav class="bg-slate-600 px-3 py-1 mb-3 rounded text-slate-200 flex gap-2">
-	<a class:active={$page.url.pathname === '/'} href="/">Welcome</a>
-	<a class:active={$page.url.pathname === '/basics'} href="/basics">Basics</a>
+<nav class="bg-slate-600 px-4 pb-1 mb-3 rounded text-slate-200 flex gap-3">
+	{#each pageItems as pageItem}
+		<a class:active={$page.url.pathname === pageItem.href} href={pageItem.href}>{pageItem.title}</a>
+	{/each}
 </nav>
 
 <style>
