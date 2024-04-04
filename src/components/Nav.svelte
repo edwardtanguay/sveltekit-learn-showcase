a9bf523cec8fdc15c7d6ec1a58dfa78ebce20288
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-04 22:57:53 +0200

    nav in function

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 
-	const pageItems = [
+	type PageItem = {
+		href: string;
+		title: string;
+	};
+
+	const pageItems: PageItem[] = [
 		{
 			href: '/',
 			title: 'Home'
@@ -9,13 +14,21 @@
 		{
 			href: '/basics',
 			title: 'Basics'
+		},
+		{
+			href: '/blog',
+			title: 'Blog'
 		}
 	];
+
+	const highlightMenuItem = (pageItem: PageItem) => {
+		return $page.url.pathname.startsWith(pageItem.href);
+	};
 </script>
 
 <nav class="bg-slate-600 px-4 pb-1 mb-3 rounded text-slate-200 flex gap-3">
 	{#each pageItems as pageItem}
-		<a class:active={$page.url.pathname === pageItem.href} href={pageItem.href}>{pageItem.title}</a>
+		<a class:active={highlightMenuItem(pageItem)} href={pageItem.href}>{pageItem.title}</a>
 	{/each}
 </nav>
 
