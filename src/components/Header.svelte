1e0476822b3e21056d9587be0e651e1ab9985f53
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 11:52:16 +0200

    icon showing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang="ts">
 	import Nav from './Nav.svelte';
+	import 'iconify-icon';
 </script>
 
-<h1 class="text-orange-800 text-[1.6rem] mb-2">SvelteKit Learn Showcase</h1>
+<div class="flex justify-between place-items-center mb-2 md:mb-1">
+	<h1 class="text-orange-800 text-[1.4rem] md:text-[1.7rem]">SvelteKit Learn Showcase</h1>
+	<iconify-icon icon="charm:menu-hamburger" class="text-2xl md:hidden"></iconify-icon>
+</div>
 <Nav />
