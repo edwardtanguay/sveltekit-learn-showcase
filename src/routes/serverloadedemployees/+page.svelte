1c67f83eff6b1acef1ec6cf0519955d85eb55d3f
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 15:58:25 +0200

    server loading done

--- src/routes/serverloadedemployees/+page.svelte
+++ src/routes/serverloadedemployees/+page.svelte
@@ -4,7 +4,7 @@
 
 <p class="mt-6 mb-4">These employees are loaded in server-side loading pages.</p>
 
-<ul class="list-disc ml-6 mt-6">
+<ul class="list-disc ml-6">
 	{#each data.employees as employee}
 		<li class="mb-2">
 			<a href={`/serverloadedemployees/${employee.employeeID}`} class="underline"
