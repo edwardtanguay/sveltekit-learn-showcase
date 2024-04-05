91f4b449119b9a44948d927b6c622e20b97d9aa8
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-05 11:35:17 +0200

    new texts on home page

--- src/components/SiteInfo.svelte
+++ src/components/SiteInfo.svelte
@@ -1,4 +1,7 @@
-<h2 class="text-xl">Site Links</h2>
+<h2 class="text-xl">Purpose</h2>
+
+<p class="ml-4 mb-3">This site is a place to try out everything I'm learning about <b>SvelteKit</b> as I take the <a href="https://frontendmasters.com/courses/sveltekit" target="_blank" class="underline">Fundamentals course</a> by Rich Harris.</p>
+<h2 class="text-xl">Links</h2>
 <ul class="list-disc ml-6">
 	<li>
 		<a
@@ -10,6 +13,11 @@
 	<li>
 		<a class="underline" href="https://sveltekit-the-learn-showcase.vercel.app" target="_blank"
 			>live site</a
+		>
+	</li>
+	<li>
+		<a class="underline" href="https://tanguay-eu.vercel.app/howtos/852" target="_blank"
+			>notes</a
 		>
 	</li>
 </ul>
