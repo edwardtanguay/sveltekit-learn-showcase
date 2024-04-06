b03196b365c6403eaccce8f1c2f9bb9a9eeedf50
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-06 17:07:41 +0200

    base 6 in

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 	import Example from '../../components/Example.svelte';
 	import Ex004ServerFileTodos from '../../components/examples/Ex004ServerFileTodos.svelte';
+	import Ex006MongoDbAtlasTodoCrud from '../../components/examples/Ex006MongoDbAtlasTodoCrud.svelte';
 </script>
 
 <section>
 	<Example title="Ex005ArrayStateVariable">
 		<Ex004ServerFileTodos />
+	</Example>
+	<Example title="Ex006MongoDbAtlasTodoCrud">
+		<Ex006MongoDbAtlasTodoCrud />
 	</Example>
 </section>
