ff5025d8db11f36fc046181a9310dfe9a8a138e2
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-06 07:29:52 +0200

    todos server working

--- src/components/examples/Ex004ServerFileTodos.svelte
+++ src/components/examples/Ex004ServerFileTodos.svelte
@@ -4,15 +4,22 @@
 	$: totalDoubled = todos.length * 2;
 
 	const addTodo = () => {
-		console.log('adding');
 		todos.push('999');
-		console.log(todos);
+		todos = structuredClone(todos);
 	};
 </script>
 
-<p class="w-fit max-w-[15rem] text-xs">The todo data is being permeated in a file. This won't work on Vercel.</p>
+<p class="w-fit max-w-[15rem] text-xs">
+	The todo data is being permeated in a file. This won't work on Vercel.
+</p>
 <button on:click={addTodo} class="bg-gray-300 px-2 mt-2 rounded border border-gray-500 w-fit"
 	>Add Todo</button
 >
+
+<ul>
+	{#each todos as todo}
+		<li>{todo}</li>
+	{/each}
+</ul>
 <p class=" pt-1">number of todos: {todos.length}</p>
 <p class=" pt-1">number of todos doubled: {totalDoubled}</p>
