3d2d8379c47d82ee67334095d27c26f0e7369d99
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-07 13:58:19 +0200

    loading in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import '../app.css';
 	import Header from '../components/Header.svelte';
+	import { navigating } from '$app/stores';
 </script>
 
-<Header/>
-<slot />
+<Header />
+
+{#if $navigating}
+	<p>loading...</p>
+{:else}
+	<slot />
+{/if}
