4783255d41f6f186d3fecd69d59f71e034d5bf58
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-07 15:56:14 +0200

    fully responsive

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -8,32 +8,44 @@
 	import { page } from '$app/stores';
 	import Ex007Stores from '../../components/examples/Ex007Stores.svelte';
 
+	let innerWidth = 0;
+
 	onMount(() => {
 		invalidateAll();
 	});
 </script>
+
+<svelte:window bind:innerWidth />
 
 <section>
 	<Example title="Ex004ServerFileTodos">
 		<Ex004ServerFileTodos />
 	</Example>
 	<Example title="Ex006MongoDbAtlasTodoCrud">
-		<div class="w-[35rem]">
+		<div class="w-full md:w-[35rem]">
 			{#key data.todos}
 				<p class="mb-3 mt-2">There are {data.todos.length} todos.</p>
 				<ul class="font-mono text-orange-900">
 					{#each data.todos as todo}
 						<li>
-							{`[${todo.id}] - ${tools.displayStars(todo.stars)} - ${todo.finished ? 'done' : 'todo'} - ${todo.title}`}
+							{#if innerWidth > 768}
+								{`[${todo.id}] - ${tools.displayStars(todo.stars)} - ${todo.finished ? 'done' : 'todo'} - ${todo.title}`}
+							{:else}
+								{todo.title}
+							{/if}
 						</li>
 					{/each}
 				</ul>
 			{/key}
-			<p class="devNote">These todos are being fetched directly from a MongoDB database in the +page.server.ts file, which accesses environment variables to get the connection strings, etc. They are refetched on onMount() i.e. when the user reloads the browser.</p>
+			<p class="devNote">
+				These todos are being fetched directly from a MongoDB database in the +page.server.ts file,
+				which accesses environment variables to get the connection strings, etc. They are refetched
+				on onMount() i.e. when the user reloads the browser.
+			</p>
 		</div>
 	</Example>
-<Example title="Ex007Stores">
-	<p>PAGE: $page.url.pathname = {$page.url.pathname}</p>
-	<Ex007Stores/>
-</Example>
+	<Example title="Ex007Stores">
+		<p>PAGE: $page.url.pathname = {$page.url.pathname}</p>
+		<Ex007Stores />
+	</Example>
 </section>
